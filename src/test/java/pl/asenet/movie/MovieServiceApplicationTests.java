792aa842f3383aa6d02c2dfe0c8c45dda7d53996
TomS <tszymeczek@gmail.com>
2019-11-28 22:35:10 +0100

    Project initialization

---
+++ src/test/java/pl/asenet/movie/MovieServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.asenet.movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
